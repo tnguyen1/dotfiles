f60ebb8dbc2d8c747845d41e31c80f0121aa20b4
Truc Nguyen <truc.nguyen@bonitasoft.com>
2014-09-08 10:37:14 +0200

    add git difftool alias + global gitignore

--- .oh-my-zsh/custom/truc.zsh
+++ .oh-my-zsh/custom/truc.zsh
@@ -1,5 +1,8 @@
 alias psg='ps aux | grep -i'
 alias ack-grep='ack-grep --color-filename=magenta --color-lineno=yellow --color-match=yellow'
-alias gahead='git rev-list --pretty=oneline origin/$(current_branch)..$(current_branch)'
 alias pp='python -m json.tool'
 alias http='python -m SimpleHTTPServer'
+
+# Git
+alias gdt='git difftool'
+alias gahead='git rev-list --pretty=oneline origin/$(current_branch)..$(current_branch)'
